50858791dad5c4af49795424b2815742c18ea2ad
MaciejKrawczyk <kontakt.maciejkrawczyk@gmail.com>
2023-10-31 14:33:37 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,37 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 
+// fn say_hello() {
+//     println!("hello");
+// }
+
+// fn get_sum(x: i32, y: i32) {
+//     println!("{} + {} = {}", x, y, x+y)
+// }
+
+// fn get_sum_2(x: i32, y: i32) -> i32 {
+//     x+y  // <-- no ';' also return x+y; works
+// }
+
+// fn get_2(x:i32) -> (i32, i32) {
+//     return (x+1, x+2)
+// }
+
+
+
 fn main() {
+
+
+    // let (v1, v2) = get_2(4);
+    // println!("{}, {}", v1, v2)
+
+    // println!("{}", get_sum_2(4,5));
+    // get_sum(5,4);
+    // say_hello();
+
+
+
+
 
     // println!("What is your name? ");
     //
@@ -158,6 +188,50 @@ fn main() {
     //     println!("{}", char);
     // }
 
+    // casting
+    // let int_u8: u8 = 5;
+    // let int2_u8: u8 = 4;
+    // let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
 
+
+    // enum Day {
+    //     Monday,
+    //     Tuesday
+    // }
+    //
+    // impl Day {
+    //     fn is_weekend(&self) -> bool {
+    //         match self {
+    //             Day::Monday | Day::Tuesday => true,
+    //             _ => false
+    //         }
+    //     }
+    // }
+    //
+    // let today: Day = Day::Monday;
+    // match today {
+    //     Day::Monday => println!("everyone hates Monday"),
+    //     Day::Tuesday => println!("Tuesday")
+    // }
+    //
+    // println!("Is today the weekend {}", today.is_weekend())
+
+    // let vec1: Vec<i32> = Vec::new();
+    // let mut vec2 = vec![1,2,3,4];
+    // vec2.push(5);
+    // println!("1st: {}", vec2[0]);
+    // let second: &i32 = &vec2[1];
+    // match vec2.get(1) {
+    //     Some(second) => println!("2nd: {}", second),
+    //     None => println!("no second value"),
+    // }
+    // for i in &mut vec2 {
+    //     *i *= 2;
+    // }
+    // for i in &vec2 {
+    //     println!("{}", i)
+    // }
+    // println!("Vec length {}", vec2.len());
+    // println!("Pop : {:?}", vec2.pop());
 
 }
